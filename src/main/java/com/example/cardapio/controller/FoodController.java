4f88bed0fcd8ce7dbea62212773190395aa4cc34
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2023-05-10 22:51:27 -0300

    feat: create class Food

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,5 +1,6 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.food.Food;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FoodController {
     @GetMapping
     public void getAll() {
-
+        Food food;
     }
 }
